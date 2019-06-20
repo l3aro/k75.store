3ccc8218ff533a65a530bf50fa860876f226ff2e
l3lackheart <dgbao1340@gmail.com>
2019-06-20 20:40:53 +0700

    Add old email input

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -26,7 +26,7 @@
 						@csrf
 						<fieldset>
 							<div class="form-group">
-								<input class="form-control" placeholder="E-mail" name="email" type="email" autofocus="">
+								<input class="form-control" placeholder="E-mail" name="email" type="email" autofocus="" value="{{ old('email') }}">
 							</div>
 							<div class="form-group">
 								<input class="form-control" placeholder="Password" name="password" type="password" value="">
